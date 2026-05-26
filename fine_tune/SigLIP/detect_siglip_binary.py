e1a12ff080d13bc1c9646630a2d0bd269a766fae
QuocKN <quocnk2o4@gmail.com>
2026-05-26 16:42:04 +0700

    result refactor and no refactor spectrogram 1toanbin

--- fine_tune/SigLIP/detect_siglip_binary.py
+++ fine_tune/SigLIP/detect_siglip_binary.py
@@ -25,9 +25,9 @@ IMAGE_EXTS = {".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tif", ".tiff"}
 
 # Edit these values directly.
 CHECKPOINT_IN = "fine_tune/SigLIP/siglip_binary_runs/siglip_binary_best.pt"
-SOURCE_DIR = "/home/quocnk/Documents/NKQuoc/Data/RF/Tu_thu/drone1/spectrograms"
-OUTPUT_JSON = "fine_tune/SigLIP/report/test/results.json"
-OUTPUT_CHART = "fine_tune/SigLIP/report/test/results_chart.png"
+SOURCE_DIR = "/home/quocnk/Documents/NKQuoc/Data/RF/Tu_thu/drone1/1toan_spectrograms_refactor"
+OUTPUT_JSON = "fine_tune/SigLIP/report/test/results_refactor.json"
+OUTPUT_CHART = "fine_tune/SigLIP/report/test/results_chart_refactor.png"
 IMAGE_SIZE = 224
 DEVICE = "cuda:0"
 BATCH_SIZE = 64
